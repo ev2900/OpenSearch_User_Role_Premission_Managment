ba5142a092d75c423318712656aa2bdd6935805c
Christopher Sharkey <ev2900@gmail.com>
2023-04-05 15:38:47 -0400

    Update removing_a_user_from_an_opensearch_role_mapping.py

--- removing_a_user_from_an_opensearch_role_mapping.py
+++ removing_a_user_from_an_opensearch_role_mapping.py
@@ -4,7 +4,7 @@ import json
 # Update the values of these variables
 opensearch_domain_endpoint = "domain endpoint ex. https://search-workshop-domain-xxxxxx.us-east-1.es.amazonaws.com"
 user_name_auth = "<user name for the user authenticating the HTTP request"
-password_auth "<password for user authenticating the HTTP request>"
+password_auth = "<password for user authenticating the HTTP request>"
 role_type = "<backend_roles OR users>"
 role_name = "<role name ex. all_access>"
 user_to_delete = "<user name or IAM ARN to mapp to the role name"
