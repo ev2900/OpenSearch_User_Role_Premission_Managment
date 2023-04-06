4d4acf2b8c9236eab3e9beb7adba7f8d1508fb3c
Christopher Sharkey <ev2900@gmail.com>
2023-04-06 08:36:45 -0400

    few changes

--- create_a_user.py
+++ create_a_user.py
@@ -2,21 +2,18 @@ import requests
 import json
 
 # Update the values of these variables
-opensearch_domain_endpoint = "domain endpoint ex. https://search-workshop-domain-xxxxxx.us-east-1.es.amazonaws.com"
-user_name_auth = "<user name for the user authenticating the HTTP request"
-password_auth = "<password for user authenticating the HTTP request>"
-user_name_for_new_user = "<user name for OpenSearch user that will be created"
+opensearch_domain_endpoint = "<domain endpoint ex. https://search-workshop-domain-xxxxxx.us-east-1.es.amazonaws.com>"
+user_name_auth = "<user name for the user authenticating the HTTP request>"
+password_auth = "<password for the user authenticating the HTTP request>"
+user_name_for_new_user = "<user name for the new user that will be created>"
+password_for_new_user = "<password for the new user the will be created. Passworkd must have 1 case, 1 upper case, 1 number, 1 special character"
 roles_to_map_user_to = ["<role_name>"]
 backend_roles_to_map_user_to = ["<role_name>"]
 
-role_type = "<backend_roles OR users>"
-role_name = "<role name ex. all_access>"
-user_to_map = "<user name or IAM ARN to mapp to the role name"
-
 # Create the User
 request_body = {
-		"password": "<password_for_new_user_must_have_1_lower_1_upper_1_number_1_special_character>", 
-		"opendistro_security_roles": role_to_map_user_to,
+		"password": password_for_new_user, 
+		"opendistro_security_roles": roles_to_map_user_to,
 		"backend_roles": backend_roles_to_map_user_to
 }
 
